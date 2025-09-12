0761f4881b0076753d0c1c85d2abf6ab3f34b030
끼로로 <roropist0527@gmail.com>
2025-09-12 08:28:02 +0000

    연관 태그가 없습니다. 라는 문구 삭제

--- src/app/keyword/page.tsx
+++ src/app/keyword/page.tsx
@@ -405,7 +405,6 @@ export default function KeywordPage() {
                 </div>
               ) : (
                 <div className="h-full flex items-center justify-center">
-                  <p className="text-muted-foreground text-center">연관 태그가 없습니다.</p>
                 </div>
               )}
             </CardContent>

731540e7b3c5f5db310e3b9c53aa0639414ca2c1
끼로로 <roropist0527@gmail.com>
2025-09-26 08:09:47 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ytimg.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },

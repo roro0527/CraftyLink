ea0ac60255c8506387a421867dbe54266eda774a
끼로로 <roropist0527@gmail.com>
2025-09-29 07:33:38 +0000

    모든 대화 한국어로 진행

--- next.config.ts
+++ next.config.ts
@@ -38,13 +38,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-   webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
-    return config;
-  },
 };
 
 export default nextConfig;

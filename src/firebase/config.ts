e2898ff75a373ccd2b2c53a1ab590404244173e3
끼로로 <roropist0527@gmail.com>
2025-10-13 08:42:09 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "craftylink",
+  "appId": "1:439428212295:web:7b44282ec320220b5c7362",
+  "apiKey": "AIzaSyBGm21LtpinX_pKIxqwtwEGNGk4BjLUmGU",
+  "authDomain": "craftylink.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "439428212295"
+};
